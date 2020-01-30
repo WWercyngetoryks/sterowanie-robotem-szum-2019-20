5c13fbb1ac74d1ea1af2225de9d69f0deef14a9b
WWercyngetoryks <48495543+WWercyngetoryks@users.noreply.github.com>
2020-01-30 16:57:46 +0100

    Update zolwik2.m

--- programy_laboratoria/zolwik2.m
+++ programy_laboratoria/zolwik2.m
@@ -1,26 +1,27 @@
  %% kolko turtlebot
-sterowanie = rospublisher('/turtle1/cmd_vel')
-pozycja = rossubscriber('/turtle1/pose');
-wiadomosc = rosmessage(sterowanie.MessageType)
  
- wiadomosc.Linear.X = 1;
+sterowanie = rospublisher('/turtle1/cmd_vel') % ustanowienie połaczenia wysyłanie
+pozycja = rossubscriber('/turtle1/pose'); %ustanowienie połaczenia odbieranie
+wiadomosc = rosmessage(sterowanie.MessageType) % ustanowienie typu wiadomości
+ 
+% wiadomosc.Linear.X = 1; % widomosci do wyslania 
 
-for t=0:0.1:10
+for t=0:0.1:10 % czs trwania symulacji
     
-wiadomosc.Angular.Z = 1;
+wiadomosc.Angular.Z = 1; % widomosci do wyslania 
 
-send(sterowanie,wiadomosc);
+send(sterowanie,wiadomosc); % wyslani wiadomosci
 pause(0.1);
 end
-%% pijany kierowca + logging
+%% jazda po sinusoidzie + logging
 sterowanie = rospublisher('/turtle1/cmd_vel')
 pozycja = rossubscriber('/turtle1/pose');
 wiadomosc = rosmessage(sterowanie.MessageType)
 
-wiadomosc.Linear.X = 1;
+wiadomosc.Linear.X = 1; % jazda na wprost
 for t=0:0.1:10
     
-wiadomosc.Angular.Z = sin(t);
+wiadomosc.Angular.Z = sin(t); % zmiana orientacji
 
 send(sterowanie,wiadomosc);
 pause(0.1);
@@ -34,10 +35,10 @@ wiadomosc = rosmessage(sterowanie.MessageType)
 %wiadomosc.Linear.X = 1;
 
 for t=0:0.1:10
- polozenie = receive(pozycja,1); 
- X=polozenie.X;
- Y=polozenie.Y;
- theta=polozenie.Theta;
+ polozenie = receive(pozycja,1);  % odczytanie polozenai robota
+ X=polozenie.X; % polozenie X
+ Y=polozenie.Y; % polozenie Y
+ theta=polozenie.Theta; % kąt
  
  v=0;
  omega=-theta;
